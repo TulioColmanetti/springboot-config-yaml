e5e92d8094ddb5f5c12d84be35f71ba20d5b8ec0
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2021-06-28 18:42:57 -0300

    Initial commit

---
+++ src/main/java/com/digitalinnovationone/springboot/SpringbootConfigYamlApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConfigYamlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootConfigYamlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalinnovationone/springboot/springbootconfigyaml/SpringbootConfigYamlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.springboot.springbootconfigyaml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConfigYamlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
